80df3a8cf1762f59415f05843b904bb48a029862
Edwin Chi <chipineiro@gmail.com>
2022-06-03 19:07:35 -0500

    first commit

---
+++ src/main/java/org/Employee.java
@@ -0,0 +1,38 @@
+package employee;
+
+public class Employee {
+    private String name;
+
+
+    private String lastName;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
